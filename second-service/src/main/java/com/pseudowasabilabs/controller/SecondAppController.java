c5ce7732b458ee04aaa5c8010a8afb83de51f9b9
pseudowasabi <pseudowasabi@kakao.com>
2024-09-15 14:57:48 +0900

    add hello messages for two sample services

---
+++ second-service/src/main/java/com/pseudowasabilabs/controller/SecondAppController.java
@@ -0,0 +1,15 @@
+package com.pseudowasabilabs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecondAppController {
+
+    @GetMapping("/hello")
+    private String hello() {
+        return "Hello second app user";
+    }
+}
