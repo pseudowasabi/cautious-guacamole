464e8814c9a9506b88b483cc0746fa136a811b0b
pseudowasabi <pseudowasabi@kakao.com>
2024-09-18 23:49:53 +0900

    add global filter

---
+++ api-gateway/src/main/java/com/pseudowasabilabs/filter/GlobalFilter.java
@@ -0,0 +1,51 @@
+package com.pseudowasabilabs.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalFilter.class);
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("GlobalFilter baseMessage: {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("GlobalFilter start - request ID: {}, request URI: {}", request.getId(), request.getURI());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("GlobalFilter end - response code: {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        // Put the configuration properties
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
