2fa98738ddffcbf05af49a54f944f8b3c1a5838b
pseudowasabi <pseudowasabi@kakao.com>
2024-09-24 00:10:47 +0900

    add replica settings to sample services

--- first-service/src/main/java/com/pseudowasabilabs/controller/FirstAppController.java
+++ first-service/src/main/java/com/pseudowasabilabs/controller/FirstAppController.java
@@ -1,17 +1,30 @@
 package com.pseudowasabilabs.controller;
 
+import com.pseudowasabilabs.FirstApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstAppController {
+
+    Environment env;
+
+    @Autowired
+    public FirstAppController(Environment env) {
+        this.env = env;
+    }
 
     private static final Logger log = LoggerFactory.getLogger(FirstAppController.class);
 
@@ -27,7 +40,9 @@ public class FirstAppController {
     }
 
     @GetMapping("/check")
-    private String check() {
-        return "Hi, check";
+    private String check(HttpServletRequest request) {
+        log.info("== Local server port: {}", request.getServerPort());
+
+        return String.format("message from first-app, port(yml): %s, port(httpservlet): %s", env.getProperty("local.server.port"), request.getServerPort());
     }
 }
--- second-service/src/main/java/com/pseudowasabilabs/controller/SecondAppController.java
+++ second-service/src/main/java/com/pseudowasabilabs/controller/SecondAppController.java
@@ -3,15 +3,22 @@ package com.pseudowasabilabs.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondAppController {
+
+    // not assigned properly
+    @Value("local.server.port")
+    private String localServerPort;
 
     private static final Logger log = LoggerFactory.getLogger(SecondAppController.class);
 
@@ -27,7 +34,9 @@ public class SecondAppController {
     }
 
     @GetMapping("/check")
-    private String check() {
-        return "Hi, check";
+    private String check(HttpServletRequest request) {
+        log.info("== Local server port: {}", request.getServerPort());
+
+        return String.format("message from second-app, port(yml): %s, port(httpservlet): %s", localServerPort, request.getServerPort());
     }
 }
